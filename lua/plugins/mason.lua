4bfd7592afaf02152fc0ef72985f1d52caea3b2b
Karol Czeryna <k@e-dot.uk>
2025-08-05 23:17:36 +0100

    add gitui support

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,6 +4,34 @@ return {
     opts = function(_, opts)
       table.insert(opts.ensure_installed, "isort")
       table.insert(opts.ensure_installed, "black")
+      table.insert(opts.ensure_installed, "gitui")
+    end,
+    keys = {
+      {
+        "<leader>gG",
+        function()
+          Snacks.terminal({ "gitui" })
+        end,
+        desc = "GitUi (cwd)",
+      },
+      {
+        "<leader>gg",
+        function()
+          Snacks.terminal({ "gitui" }, { cwd = LazyVim.root.get() })
+        end,
+        desc = "GitUi (Root Dir)",
+      },
+    },
+    init = function()
+      -- delete lazygit keymap for file history
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LazyVimKeymaps",
+        once = true,
+        callback = function()
+          pcall(vim.keymap.del, "n", "<leader>gf")
+          pcall(vim.keymap.del, "n", "<leader>gl")
+        end,
+      })
     end,
   },
 }

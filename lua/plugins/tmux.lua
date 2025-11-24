5a46e8038d4d3e6d39973d920ea0a8cc2bb8fa16
Karol Czeryna <k@e-dot.uk>
2025-11-24 10:52:45 +0000

    Refactor tmux

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,19 +1,20 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = function()
-      if vim.env.TMUX then
-        vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
-          callback = function()
-            vim.fn.system("tmux rename-window " .. vim.fn.expand("%:t"))
-          end,
-        })
-        vim.api.nvim_create_autocmd("VimLeave", {
-          callback = function()
-            vim.fn.system("tmux rename-window zsh")
-          end,
-        })
-      end
-    end,
-  },
+  "LazyVim/LazyVim",
+  opts = function()
+    if not vim.env.TMUX then
+      return
+    end
+
+    vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained" }, {
+      callback = function()
+        vim.fn.system("tmux rename-window " .. vim.fn.expand("%:t"))
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("VimLeave", {
+      callback = function()
+        vim.fn.system("tmux rename-window zsh")
+      end,
+    })
+  end,
 }

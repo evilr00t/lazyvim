78ab86923781253b1fe55a33e015959f9e3f8173
Karol Czeryna <k@e-dot.uk>
2025-11-24 10:53:22 +0000

    Refactor plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,20 +1,14 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
--- vim.g.lazyvim_python_lsp = "ruff"
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        pyright = {
-          mason = false,
-          autostart = false,
-        },
-        basedpyright = {
-          mason = false,
-          autostart = false,
-        },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      pyright = {
+        mason = false,
+        autostart = false,
+      },
+      basedpyright = {
+        mason = false,
+        autostart = false,
       },
     },
   },
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,37 +1,37 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "isort")
-      table.insert(opts.ensure_installed, "black")
-      table.insert(opts.ensure_installed, "gitui")
-    end,
-    keys = {
-      {
-        "<leader>gG",
-        function()
-          Snacks.terminal({ "gitui" })
-        end,
-        desc = "GitUi (cwd)",
-      },
-      {
-        "<leader>gg",
-        function()
-          Snacks.terminal({ "gitui" }, { cwd = LazyVim.root.get() })
-        end,
-        desc = "GitUi (Root Dir)",
-      },
+  "mason-org/mason.nvim",
+  opts = function(_, opts)
+    vim.list_extend(opts.ensure_installed, {
+      "isort",
+      "black",
+      "gitui",
+    })
+  end,
+  keys = {
+    {
+      "<leader>gG",
+      function()
+        Snacks.terminal({ "gitui" })
+      end,
+      desc = "GitUi (cwd)",
     },
-    init = function()
-      -- delete lazygit keymap for file history
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyVimKeymaps",
-        once = true,
-        callback = function()
-          pcall(vim.keymap.del, "n", "<leader>gf")
-          pcall(vim.keymap.del, "n", "<leader>gl")
-        end,
-      })
-    end,
+    {
+      "<leader>gg",
+      function()
+        Snacks.terminal({ "gitui" }, { cwd = LazyVim.root.get() })
+      end,
+      desc = "GitUi (Root Dir)",
+    },
   },
+  init = function()
+    -- delete lazygit keymap for file history
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyVimKeymaps",
+      once = true,
+      callback = function()
+        pcall(vim.keymap.del, "n", "<leader>gf")
+        pcall(vim.keymap.del, "n", "<leader>gl")
+      end,
+    })
+  end,
 }
